2542c73c509b297edf96d21ef4b0d435745c46fb
Richard Beare <Richard.Beare@gmail.com>
2011-05-18 16:58:19 +1000

    basics done

--- testDirectional.cxx
+++ testDirectional.cxx
@@ -1,23 +1,43 @@
 #include "ioutils.h"
 #include "itkDirectionalGradientImageFilter.h"
+#include "itkThresholdImageFilter.h"
 
 int main(int argc, char * argv[])
 {
   const unsigned dim = 2;
 
   typedef itk::Image<unsigned char, dim> ImageType;
+  typedef itk::Image<float, dim> FloatImageType;
   typedef itk::Image<unsigned char, dim> MaskType;
 
   ImageType::Pointer raw = readIm<ImageType>(argv[1]);
   MaskType::Pointer mask = readIm<MaskType>(argv[2]);
 
-  typedef itk::DirectionalGradientImageFilter<ImageType, MaskType, ImageType> DGType;
+  typedef itk::DirectionalGradientImageFilter<ImageType, MaskType, FloatImageType> DGType;
 
   DGType::Pointer dg = DGType::New();
 
   dg->SetInput(raw);
   dg->SetMaskImage(mask);
+  dg->SetOutsideValue(1);
+
+  typedef itk::ThresholdImageFilter <FloatImageType>  ThreshType;
+
+  ThreshType::Pointer thresh = ThreshType::New();
+
+  // save the dark to light transition
+  thresh->SetInput(dg->GetOutput());
+  thresh->ThresholdBelow(0);
+  thresh->SetLower(0);
+  writeIm<FloatImageType>(thresh->GetOutput(), argv[3]);
+
+  // save light to dark transition
+  dg->SetScale(-1);
+  thresh->SetInput(dg->GetOutput());
+  thresh->ThresholdBelow(0);
+  thresh->SetLower(0);
+  writeIm<FloatImageType>(thresh->GetOutput(), argv[4]);
 
 
-  return EXIT_SUCCESS
+  return EXIT_SUCCESS;
 }
